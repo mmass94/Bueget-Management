47f7e1bdc168233b9a29a5869880eae5a124afae
mmass94 <ce.m.mas.94@gmail.com>
2023-05-17 19:28:52 +0300

    Aggregating Expenses from Redux store to get it ready for Barchart

--- budget-management/src/components/visulization/barChart.js
+++ budget-management/src/components/visulization/barChart.js
@@ -7,62 +7,54 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
-  ResponsiveContainer,
 } from "recharts";
+import { useSelector } from "react-redux";
 
 const data = [
   {
     name: "Page A",
     uv: 4000,
-    pv: 2400,
-    amt: 2400,
   },
   {
     name: "Page B",
     uv: 3000,
-    pv: 1398,
-    amt: 2210,
   },
   {
     name: "Page C",
     uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
   },
 ];
 
-const barChart = () => {
+const BarChartComponent = () => {
+  const Expenses = useSelector((state) => state.expense.expenses);
+  // console.log("Expenses is", Expenses);
+  // const modi = Expenses.map(({ amount, category }) => ({
+  //   category,
+  //   amount: parseFloat(amount),
+  // }));
+
+  const aggregatedExpenses = Expenses.reduce(
+    (acc, { amount, category, ...rest }) => {
+      acc[category] = acc[category] || { amount: 0, category, ...rest };
+      acc[category].amount += parseInt(amount);
+      return acc;
+    },
+    {}
+  );
+
+  const resultArray = Object.values(aggregatedExpenses);
+
+  console.log(resultArray);
+
+  // console.log("data is ", modi);
+
   return (
     <div>
       <div>hiii</div>
       <BarChart
         width={500}
         height={300}
-        data={data}
+        data={resultArray}
         margin={{
           top: 5,
           right: 30,
@@ -71,15 +63,15 @@ const barChart = () => {
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="category" />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Bar dataKey="pv" fill="#8884d8" />
-        <Bar dataKey="uv" fill="#82ca9d" />
+        {/* <Bar dataKey="pv" fill="#8884d8" /> */}
+        <Bar dataKey="amount" fill="#82ca9d" />
       </BarChart>
     </div>
   );
 };
 
-export default barChart;
+export default BarChartComponent;
